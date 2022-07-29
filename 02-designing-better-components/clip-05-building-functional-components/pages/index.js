7ddac762c9c0ee1d73249578cf28e53862942b6c
KatieGott <Katie.Gott@target.com>
2022-07-29 14:53:56 -0500

    finished component update

--- 02-designing-better-components/clip-05-building-functional-components/pages/index.js
+++ 02-designing-better-components/clip-05-building-functional-components/pages/index.js
@@ -1,20 +1,22 @@
 import { data } from "../SpeakerData"
 
-const IndexPage = () => {
-
-  const {id,bio,first,last,favorite,twitterHandle,company,sessions,} = data[0];
-
+function Session({ title, room }) {
   return (
-    <div className="container speakers-list">
-      <div className="row">
-        {data.map(function (speaker) {
-          const {id,bio,first,last,favorite,twitterHandle,company,sessions,} = speaker;
-
-          return (
-      
-<div key={id} className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
-          <div className="card card-height p-4 mt-4">
-            <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+    <span className="session w-100">
+    {title} <strong>Room: {room.name}</strong>
+  </span>
+  )
+}
+function Sessions({ sessions }) {
+  return (
+    <div className="sessionBox card h-250">
+    <Session {...sessions[0]} />
+  </div>
+  )
+}
+function SpeakerImage({ id, first, last}) {
+  return (
+    <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
               <img 
                 className="contain-fit"
                 src={`/images/speaker-${id}.jpg`}
@@ -22,30 +24,49 @@ const IndexPage = () => {
                 alt={`${first} $(last)`}
               />
             </div>
-            <div className="speaker-info">
-              <div className="d-flex justify-content-between mb-3">
-                <h3 className="text-truncate w-200">
-                  {first} {last}
-                </h3>
-              </div>
-              <div>
-                <p> {bio} {company} {twitterHandle}</p>
-              </div>
+  )
+}
+function SpeakerDemographics({first, last, bio, company, twitterHandle, favorite}) {
+  return (
+    <div className="speaker-info">
+    <div className="d-flex justify-content-between mb-3">
+      <h3 className="text-truncate w-200">
+        {first} {last}
+      </h3>
+    </div>
+    <div>
+      <p> {bio} {company} {twitterHandle}</p>
+    </div>
+    </div>
+  )
+}
+function Speaker({ speaker }) {
+  const { id, first, last, sessions } = speaker;
+  return (
+    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+          <div className="card card-height p-4 mt-4">
+            <SpeakerImage id={id} first={first} last={last} />
+            <SpeakerDemographics {...speaker} />
             </div>
-            <div className="sessionBox card h-250">
-              <span className="session w-1">
-                {sessions[0].title} <strong>Room: {sessions[0].room.name}</strong>
-              </span>
-            </div>
+            <Sessions sessions={sessions} />
           </div>
-        </div>
+  )
+}
 
+const IndexPage = () => {
 
-          )
+  return (
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map(function (speaker) {
+        
+          return (
+            <Speaker key={speaker.id} speaker={speaker} />
+          );
         })}
             </div>
     </div>  
-  )
+  );
 };
 
 export default IndexPage;
