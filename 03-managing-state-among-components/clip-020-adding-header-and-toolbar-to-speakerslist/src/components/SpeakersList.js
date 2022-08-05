6d052a30fa480ecd6fc7390b99957ce3c1129663
KatieGott <Katie.Gott@target.com>
2022-08-05 16:04:16 -0500

    working code

--- 03-managing-state-among-components/clip-020-adding-header-and-toolbar-to-speakerslist/src/components/SpeakersList.js
+++ 03-managing-state-among-components/clip-020-adding-header-and-toolbar-to-speakerslist/src/components/SpeakersList.js
@@ -4,41 +4,12 @@ import { useState, useEffect } from 'react';
 import ReactPlaceHolder from 'react-placeholder';
 
 function SpeakersList({ showSessions }) {
-  const [speakersData, setSpeakersData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasErrored, setHasErrored] = useState(false);
-  const [error, setError] = useState("");
+  const {
+    speakerRecUpdated, isLoading,
+    hasErrored, error,
+    onFavoriteToggle,
+  } = useRequestSpeakers(2000)
 
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-  useEffect(() => {
-    async function delayFunc() {
-      try {
-    await delay(2000);
-    setIsLoading(false);
-    setSpeakersData(data);
-    } catch (e) {
-      setIsLoading(false);
-      setHasErrored(true);
-      setError(e);
-    }
-  }
-    delayFunc();
-  }, [])
-
-  function onFavoriteToggle(id) {
-    const speakersRecPrevious = speakersData.find(function (rec) {
-      return rec.id === id;
-    });
-    const speakerRecUpdated = {
-      ...speakersRecPrevious,
-      favorite: !speakersRecPrevious.favorite 
-    };
-    const speakersDataNew = speakersData.map(function (rec) {
-      return rec.id === id ? speakerRecUpdated : rec;
-    });
-    setSpeakersData(speakersDataNew);
-  }
  if (hasErrored === true) {
   return (
     <div className="text-danger">
